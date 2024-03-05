92b276fde9b6c57744646c4e1e424ec7a808a29a
Le Duc Thai <leducthai1008@gmail.com>
2024-03-05 15:20:09 +0700

    phan quyen menu

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -1,5 +1,6 @@
 const systemConfig = require("../../config/system");
 const Account = require("../../model/account.model");
+const Role = require("../../model/role.model")
 
 module.exports.requireAuth = async(req, res, next) => {
     const token = req.cookies.token;
@@ -14,13 +15,20 @@ module.exports.requireAuth = async(req, res, next) => {
             token: token,
             deleted: false,
             status: "active" 
-        });
+        }).select("-password");
 
         if(!user)
         {
             res.redirect(`/${systemConfig.prefixAdmin}/auth/login`);
             return;
         }
+
+        const role = await Role.findOne({
+            _id: user.role_id
+        })
+
+        res.locals.user = user;
+        res.locals.role = role;
 
         next();
 
