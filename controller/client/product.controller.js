35eb1782d00fd85f91b5db02e4ec56b043725c15
Le Duc Thai <leducthai1008@gmail.com>
2024-03-07 17:12:46 +0700

    trang client - menu phan cap tinh

--- controller/client/product.controller.js
+++ controller/client/product.controller.js
@@ -1,6 +1,27 @@
-//[GET]/products
-module.exports.index = (req, res) => {
-    res.render("client/pages/product/index.pug", {
-        Title: "Products"
+const Product = require("../../model/product.model")
+
+// [GET] /products
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    }).sort({
+        position:"desc"
     });
-};
+
+    for (const item of products) {
+        item.priceNew = Math.ceil((item.price * (1 - item.discountPercentage/100)));
+    }
+
+    console.log(products);
+
+    res.render("client/pages/product/index", {
+        title: "Trang danh sách sản phẩm",
+        products: products
+    });
+}
+
+//[GET] /products/detail
+module.exports.detail = (req, res) => {
+    res.send("Trang chi tiet san pham");
+}
